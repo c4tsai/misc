cb5df5e8c5050d3f66e2a2c463a6dae4aeb3326a
c4tsai <c4tsai@uwaterloo.ca>
2019-02-08 21:32:57 -0500

    Ordering of units

--- Projects/Ample/Ing.py
+++ Projects/Ample/Ing.py
@@ -2,28 +2,51 @@
 
 import csv
 import re
+import pandas as pd
 
 f1 = open("Ingredient Description Sub-Sample [Sanitized] - Country A Vendors.csv")
 f2 = open("Ingredient Description Sub-Sample [Sanitized] - Country B Vendors.csv")
 
-def split_packsize(str): 
-    out = re.split(r'(\/|-|(?<=\d)x|\sx\s)', str, maxsplit = 1)
-    if len(out) == 1: # In the event of not finding the delimiters 
-        out = [None, None] + out
-    div = re.split(r'(\D+$)', out[2], maxsplit = 1)
-    out = out[0:2] + div[0:2]
+def str_to_num(strg, flt = False):
+    try:
+        if flt:
+            out = float(strg)
+        else: 
+            out = int(strg)
+    except ValueError:
+        out = strg
     return out
 
-def parse_csv(f, dest = "output.csv"):
+def split_packsize(strg): 
+    temp = re.split(r'(\/|-|(?<=\d)x|\sx\s)', strg, maxsplit = 1)
+    if len(temp) == 1: # In the event of not finding the delimiters 
+        temp = [None, None] + temp
+    div = re.split(r'(\D+$)', temp[2], maxsplit = 1)
+    if len(div) == 1: 
+        div = [None] + div
+    temp = temp[0:2] + div[0:2]
+    out = []
+    for i in temp: 
+        try:
+            out.append(i.strip().lower()) # Removes leading whitespace
+        except AttributeError:
+            out.append('')
+    return out
+
+def parse_csv(f):
     data = csv.reader(f)
-    fout = open(dest, mode = 'w', newline = '')
-    writer = csv.writer(fout)
+    out = []
     for row in data:
         if re.search(r'^\d{3,}$', row[1]) is not None:
-            writer.writerow([row[1]] + split_packsize(row[0]))
+            out.append([row[1]] + split_packsize(row[0]))
         else:
-            writer.writerow([row[0]] + split_packsize(row[1]))
-    fout.close()
+            out.append([row[0]] + split_packsize(row[1]))
+    return out;
         
-parse_csv(f1, "output_countryA_1.csv")
-parse_csv(f2, "output_countryB_1.csv")
+
+data1 = pd.DataFrame(parse_csv(f1)[1:], columns = ["Description", "num1", "div", "num2", "unit"])
+data2 = pd.DataFrame(parse_csv(f2)[1:], columns = ["Description", "num1", "div", "num2", "unit"])
+
+print(data1.groupby(['unit']).count().sort_values("div", ascending=False))
+data1.to_csv("output_countryA_2.csv")
+data2.to_csv("output_countryB_2.csv")
