5afa68ec8cfbef5d8177f597fac34ca9c097e4a8
c4tsai <c4tsai@uwaterloo.ca>
2019-02-11 10:35:39 -0500

    added taking care of highest level unit name

--- Projects/Ample/Ing.py
+++ Projects/Ample/Ing.py
@@ -18,14 +18,19 @@ def str_to_num(strg, flt = False):
     return out
 
 def split_packsize(strg): 
-    temp = re.split(r'(\/|-|(?<=\d)x|\sx\s)', strg, maxsplit = 1)
+    temp = re.split(r'(?<=(let|ase|bag))(.)', strg, maxsplit = 1, flags = re.IGNORECASE)
+    if len(temp) == 1:
+        temp = strg
+    else: 
+        temp = temp[3]
+    temp = re.split(r'(\/|-|(?<=\d)x|\sx\s)', temp, maxsplit = 1)
     if len(temp) == 1: # In the event of not finding the delimiters 
         temp = [None, None] + temp
     div = re.split(r'(\D+$)', temp[2], maxsplit = 1)
     if len(div) == 1: 
         div = [None] + div
     temp = temp[0:2] + div[0:2]
-    out = []
+    out = [strg]
     for i in temp: 
         try:
             out.append(i.strip().lower()) # Removes leading whitespace
@@ -44,9 +49,9 @@ def parse_csv(f):
     return out;
         
 
-data1 = pd.DataFrame(parse_csv(f1)[1:], columns = ["Description", "num1", "div", "num2", "unit"])
-data2 = pd.DataFrame(parse_csv(f2)[1:], columns = ["Description", "num1", "div", "num2", "unit"])
+data1 = pd.DataFrame(parse_csv(f1)[1:], columns = ["Description", "original", "num1", "div", "num2", "unit"])
+data2 = pd.DataFrame(parse_csv(f2)[1:], columns = ["Description", "original", "num1", "div", "num2", "unit"])
 
 print(data1.groupby(['unit']).count().sort_values("div", ascending=False))
-data1.to_csv("output_countryA_2.csv")
-data2.to_csv("output_countryB_2.csv")
+data1.to_csv("output_countryA_3.csv")
+data2.to_csv("output_countryB_3.csv")
